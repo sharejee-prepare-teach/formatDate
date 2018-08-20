fb0cc9e293dee418a2e52ff42b1c64761257e9a2
Rith <rithronlkh@gmail.com>
2018-08-20 16:06:35 +0700

    add sample format date

--- src/Main.java
+++ src/Main.java
@@ -4,35 +4,59 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Main {
-
+    public static String PATTERN = "E MMM dd HH:mm:ss Z yyyy";
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
         String dateFrom = "Mon Jun 18 00:00:00 IST 2012";
         String dateTo = "Mon Jun 18 00:00:00 IST 2018";
-        String dateF = dateFrom.toString();//"Mon Jun 18 00:00:00 IST 2012";
+        String dateF = dateFrom.toString();
         String dateT = dateTo.toString();
-        DateFormat formatter = new SimpleDateFormat("E MMM dd HH:mm:ss Z yyyy");
-        DateFormat formatIReportSQL = new SimpleDateFormat("dd/MM/yyyy");
         Date df = null;
         Date dt = null;
 
-        Date df2 = null;
-        Date dt2 = null;
-        try {
-            df = (Date)formatter.parse(dateF);
-            dt = (Date)formatter.parse(dateT);
-            System.out.println("--------DateFrom:------ "+df);
-            System.out.println("--------DateTo:------ "+dt);
+        df = getDateValue(dateF);
+        dt = getDateValue(dateT);
+        System.out.println("--------DateFrom:------ "+df);
+        System.out.println("--------DateTo:------ "+dt);
 
-            String dfs = new SimpleDateFormat("dd-MM-yyyy").format(df);
-            String dts = new SimpleDateFormat("dd-MM-yyyy").format(dt);
-            System.out.println("--------DateFrom:------ " + dfs);
-            System.out.println("--------DateTo:------ " + dts);
+        String dfs = getDateFormatToSQL(df);
+        String dts = getDateFormatToSQL(dt);
+        System.out.println("--------DateFrom:------ " + dfs);
+        System.out.println("--------DateTo:------ " + dts);
 
-        } catch (ParseException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+    }
+
+    public static String getDateFormatToSQL(Date date) {
+        if (date != null) {
+            String dateFormat = new SimpleDateFormat("dd-MMM-yyyy").format(date);
+            return dateFormat;
         }
+        return null;
+    }
+
+    public static Date getDateValue(String dateString) {
+        //"Mon Jun 18 00:00:00 IST 2012";
+        if (dateString != null) {
+            DateFormat formatter = new SimpleDateFormat(PATTERN);
+            Date date = null;
+            try {
+                date = (Date) formatter.parse(dateString);
+                return date;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    public static String getDateFromFormat(String dateString) {
+        if (dateString != null) {
+            Date date = null;
+            date = getDateValue(dateString);
+            String dfs = getDateFormatToSQL(date);
+            return dfs;
+        }
+        return null;
     }
 }
